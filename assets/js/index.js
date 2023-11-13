0ba992b79ff2b74b0994ea56a4cc01f9c21719e0
CazBella <144620823+cazbella@users.noreply.github.com>
2023-11-13 20:02:09 +0000

    Update index.js
    Begins to pseudocode challenge 08

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,33 @@
+To create the weather dashboard, you'll need to use the OpenWeatherMap API to retrieve weather data based on city coordinates. You can obtain coordinates by making a separate API call to the Geocoding API, using the city name.
 
+Here's a general guide on how to proceed:
+
+1. //Get API Key
+
+2. //Set Up HTML and CSS
+   //Create a form for city input and a section to display weather information.
+   //Style your dashboard using css
+
+3. //JavaScript 
+   //handle user input and make API requests.
+   //Retrieve city coordinates by making a Geocoding API call using the city name.
+
+4. //Fetch Weather Data
+   //Use the obtained coordinates to make a request to the 5 Day Weather Forecast API.
+   //Extract and display relevant information like city name, date, weather conditions, temperature, humidity, and wind speed.
+
+5. //LocalStorage for Search History
+   //Store the searched cities in the localStorage
+   //Display the search history on the dashboard?
+
+6. //Display 5-Day Forecast
+   //Parse the API response to show the 5-day forecast with date, weather conditions, temperature, and humidity
+
+7. //Handle Search History Click
+   //Implement functionality to handle clicks on the search history
+   //Display the weather information for the selected city
+
+8. //autofill for city names? is there a way to get a list of names to correspond with weather api? 
+//make cards for each day dynamically createded using jquery. 
+
+9. //responsiveness - use percentages and media queries and flexbox or dynamically create bootstrap elements.  
