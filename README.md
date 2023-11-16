6888f477ab9b2b7ccf5959b8b196cfe5ce9534f3
CarolineBellaLane <carolinelane@live.co.uk>
2023-11-16 12:59:11 +0000

    adds some styling, adds event listener for search button, adds local storage save function

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # weather-forecast-dashboard
 
 A weather dashboard that runs in the browser and feature dynamically updated HTML and CSS.
+
+# Usage
+
+Create a weather dashboard with form inputs.
+
+When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history.
+
+When a user views the current weather conditions for that city they are presented with:
+
+The city name
+The date
+An icon representation of weather conditions
+The temperature
+The humidity
+The wind speed
+When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
+The date
+An icon representation of weather conditions
+The temperature
+The humidity
+
+When a user clicks on a city in the search history they are again presented with current and future conditions for that city.
+
+Mock-Up
+The following image shows the web application's appearance and functionality:
+
+The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for London.
+
+# References 
+reset css stylesheet
+https://meyerweb.com/eric/tools/css/reset/
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,87 @@
-To create the weather dashboard, you'll need to use the OpenWeatherMap API to retrieve weather data based on city coordinates. You can obtain coordinates by making a separate API call to the Geocoding API, using the city name.
+var currentDate = dayjs(); // dayjs() gets today's date
+  console.log(currentDate)
 
-Here's a general guide on how to proceed:
+  var APIKey = "66e478d3790b2fa410a64b9ae201d036";
+  var queryURL;
+  //need weather we address +API key?
 
+  var rightNow = dayjs().format('D/MM/YY');
 1. //Get API Key
+   //66e478d3790b2fa410a64b9ae201d036
 
-2. //Set Up HTML and CSS
-   //Create a form for city input and a section to display weather information.
-   //Style your dashboard using css
+2. //stye css as demo.png in class module
 
 3. //JavaScript 
    //handle user input and make API requests.
    //Retrieve city coordinates by making a Geocoding API call using the city name.
 
 4. //Fetch Weather Data
+//fetch code from class
+///fetch(queryURL)
+   //.then(function (response) {
+      //return Response.json();
+//    //}).then (function (data) {
+//       console.log(data);
+//       // Create and save a reference to new empty table row
+//can I do this with bootstrao cards????????? 1 for each day?
+//       var row = $("<tr>");
+//       // Create and save references to 3 td elements containing the Title, Year, and Actors from the Fetch response object
+//       var title=$("<td>" + data.Title + "</td>");
+//       var year=$("<td>" + data.Year + "</td>");
+//       var actors=$("<td>" + data.Actors + "</td>");
+//       // Append the td elements to the new table row
+//       row.append(title).append(year).append(actors);
+//       // Append the table row to the tbody element
+//       $("tbody").append(row);
+//     }).catch(function (err) {
+//       console.log("Oh no! This happened!", err);
+//     });
+// }
+
+
+
    //Use the obtained coordinates to make a request to the 5 Day Weather Forecast API.
    //Extract and display relevant information like city name, date, weather conditions, temperature, humidity, and wind speed.
 
 5. //LocalStorage for Search History
    //Store the searched cities in the localStorage
    //Display the search history on the dashboard?
+   //setItem and getItem 
+
+   //code similar to last challenge to local storage
+   function saveCityInput(city) {
+      
+      //if no data - empty object
+      var cityHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+      
+      //push data to searchHistory array
+      cityHistory.push(city);
+
+      localStorage.setItem("searchHistory", JSON.stringify(cityHistory));
+   }
+   
+// Event listener for the search button
+$("#search-button").on("click", function() {
+   var city = $("#search-input").val();
+
+   // Perform the search 
+   console.log("Search for: " + city);
+
+   // Call function to save the city to local storage
+   saveCityInput(city);
+
+   //clear search box after searching 
+   $("#search-input").val("");
+
+});
+
 
 6. //Display 5-Day Forecast
    //Parse the API response to show the 5-day forecast with date, weather conditions, temperature, and humidity
 
 7. //Handle Search History Click
    //Implement functionality to handle clicks on the search history
+   //dynamically create buttons 'in the other div' '.list-group #history' that can also be clicked with event listener to fetch api and append/display 5 day forecast
    //Display the weather information for the selected city
 
 8. //autofill for city names? is there a way to get a list of names to correspond with weather api? 
