c5c5f53ee4a827337cd6cf9e98149365b490ce38
CarolineBellaLane <carolinelane@live.co.uk>
2023-11-19 19:26:17 +0000

    finishes readme and tidies pseudocode

--- README.md
+++ README.md
@@ -2,40 +2,63 @@
 
 A weather dashboard that runs in the browser and feature dynamically updated HTML and CSS.
 
-# Intention??
-One of the hardest things was getting used to API keys and dealing with security issues. 
+https://cazbella.github.io/weather-forecast-dashboard/
 
-# Usage
+## Usage
 
-Create a weather dashboard with form inputs.
+Upon loading the application the user is faced with the following page. 
 
-When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history.
+![Screenshot of landing area](assets/images/screenshot-landing-area-weather-dashboard.png)
+
+1. There is a form input for the current city. 
+
+2. When a user searches for a city and presses enter/click's search, they are presented with current and future conditions for that city and that city is added to the search history. This is shown in the screenshot below. 
+
+![Screenshot of weather conditions now and a forecast](assets/images/screenshot-weather-search-showing-history.png)
 
 When a user views the current weather conditions for that city they are presented with:
 
-The city name
-The date
-An icon representation of weather conditions
-The temperature
-The humidity
-The wind speed
-When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
-The date
-An icon representation of weather conditions
-The temperature
-The humidity
+* The city name
+* The date
+* An icon representation of weather conditions
+* The temperature
+* The humidity
+* The wind speed
 
-When a user clicks on a city in the search history they are again presented with current and future conditions for that city.
+When a user views future weather conditions for that city they are presented with a 5-day forecast underneath the weather now that displays:
 
-Mock-Up
-The following image shows the web application's appearance and functionality:
+* The date
+* An icon representation of weather conditions
+* The maxumim temperature
+* The minimum temperature
+* The humidity
 
-The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for London.
+3. When a user clicks on a city in the search history they are again presented with current and future conditions for that city.
+
+## Challenges
+
+I have been getting used to working with APIs and have found this new dimension challenging this week. Please see the information below for more details. 
+
+__A Note about the API key__
+_____________________________
+
+I had a few issues as my repository contains an API key. I am aware that this is a security risk and that we need to learn in future how to ensure that this sensitive informtion is not committed to GitHub. In future I will use a GitIgnore file to hide this. As no card details were provided this time, I will just deactivate the keys after use. 
+
 
 __A Note about Bootstrap__
 _____________________________
 
-Although we studied bootstrap in class, the implications of classes and versions are just becomming apparent to me by working with it. I need to work further with the classes and versions on this project in order to learn more about bootstrap and responsiveness. 
+Although we studied bootstrap in class, the implications of classes and versions are just becomming apparent to me by working with it. I need to work further with the classes and versions on this project in order to learn more about bootstrap and responsiveness. I have learned that the classes provided in the starter code are bootstrap 3/4 classes, and when I contacted Ask BCS they provided me with the CDN for bootstrap 3. I have been unable to make the project as responsive as I would have liked, but I do feel more informed about how to implement this in the future after reading more about it. 
+
+__A Note about Improvements__
+_____________________________
+
+In future I would like to add code that enables the user to 'set home city' and for this to load every time when the user views the page. I would also like to improve location information so that the correct information is always displayed. I would also like to add a function to capitlise input using .toUpperCase and .slice. I would also like to investigate making an autocomplete form for the search box. I have researched both of these and they are possible, but I didn't want to copy large chunks of code from the web so want to practise with these things first. 
+
+__A Note about Pseudocode__
+_____________________________
+
+The JavaScript files that contain this code contains lots of comments. The tutor was asked if this was good practice and the advice given was that as I am new to web development, there should be lots of comments at this stage. Hopefully the comments and the commits explain the process I followed to complete the assignment. I have also made notes here to help me remember the process and what the syntax means. 
 
 # References 
 reset css stylesheet
@@ -43,3 +66,18 @@ https://meyerweb.com/eric/tools/css/reset/
 https://getbootstrap.com/docs/5.0/components/buttons/
 https://stackoverflow.com/questions/73672814/dynamic-bootstrap-cards-with-javascript
 https://www.pluralsight.com/blog/data-professional/using-jquery-css-method
+https://stackoverflow.com/questions/40806692/dynamically-create-rows-and-columns-in-bootstrap-rails
+https://www.geeksforgeeks.org/difference-between-bootstrap-3-and-bootstrap-4/
+https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+
+I also used some code from our tutor session this week. 
+
+## Contributing
+
+I would very much appreciate some feedback on the methods I have used to achieve the task. Is this the most concise code I could have used? I would also like to know if the readme layout and content was appropriate for the task if possible please. 
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,75 +3,49 @@ var apiKey = "c44f9b2e93a173784fe4ba28b66d5418";
 
 console.log("API Key: " + apiKey);
 
-
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "London" + "&appid=" + apiKey;
-
-
 // // var defaultCity = 'London'???
 // upon page load???
-// var iconCode = data.weather[0].icon;???
 //coordinates????
 
-4. //Fetch Weather Data
-   //Use the obtained coordinates to make a request to the 5 Day Weather Forecast API.
-   //parse and display relevant information like city name, date, weather conditions, temperature, humidity, and wind speed.
+//code similar to last challenge to local storage
+function saveCityInput(city) {
 
-   //code similar to last challenge to local storage
-   function saveCityInput(city) {
-      
-      //if no data - empty object
-      var cityHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-      
-      //push data to searchHistory array
-      cityHistory.push(city);
+   //if no data - empty object
+   var cityHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
-      localStorage.setItem("searchHistory", JSON.stringify(cityHistory));
-   }
+   //push data to searchHistory array
+   cityHistory.push(city);
 
-   //load history to keep buttons on the page
-   $(document).ready(function () {
-      // var cityHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-  
-      // // Loop through the search history and create buttons for each city
-      // for (var i = 0; i < cityHistory.length; i++) {
-      //     var historyButton = $("<button>")
-      //         .addClass("btn btn-secondary history-button")
-      //         .text(cityHistory[i])
-      //         .on("click", function (event) {
-      //          //stops page refreshing here
-      //          event.preventDefault();
-      //          // button click event for history buttons
-      //          var cityName = $(this).text();
-      //          //runs function based on 
-      //          getCurrentWeather(cityName);
-      //       });
-  
-      //     // Append the button to the search history container
-      //     $("#history").prepend(historyButton);
-      // }
-      // Load history to keep buttons on the page
-var cityHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-
-// Loop through the search history and create buttons for each city
-for (var i = 0; i < cityHistory.length; i++) {
-    var historyButton = $("<button>")
-        .addClass("btn btn-secondary history-button")
-        .text(cityHistory[i]);
-
-    // Append the button to the search history container
-    $("#history").prepend(historyButton);
+   localStorage.setItem("searchHistory", JSON.stringify(cityHistory));
 }
-$("#history").on("click", ".history-button", function (event) {
-   event.preventDefault();
-   var cityName = $(this).text();
-   getCurrentWeather(cityName);
-   getWeatherForecast(cityName);
+
+//load history to keep buttons on the page
+$(document).ready(function () {
+
+   var cityHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+   // Loop through the search history and create buttons for each city
+   //from activity this week
+   for (var i = 0; i < cityHistory.length; i++) {
+      var historyButton = $("<button>")
+         .addClass("btn btn-secondary history-button")
+         .text(cityHistory[i]);
+
+      // Append the button to the search history container
+      $("#history").prepend(historyButton);
+   }
+   $("#history").on("click", ".history-button", function (event) {
+      event.preventDefault();
+      var cityName = $(this).text();
+      getCurrentWeather(cityName);
+      getWeatherForecast(cityName);
+   });
 });
-  });
-  
-   
+
+
 // Event listener for the search button
-$("#search-button").on("click", function(event) {
+$("#search-button").on("click", function (event) {
+   //prevent default stops browser from clearing info
    event.preventDefault();
    console.log("search button clicked")
    var city = $("#search-input").val();
@@ -87,14 +61,14 @@ $("#search-button").on("click", function(event) {
 
    //make a button with each search
    var historyButton = $("<button>")
-        .addClass("btn btn-secondary history-button")
-        .text(city);
- console.log(historyButton);
-    // Append the button to the search history container
-    $("#history").prepend(historyButton);
+      .addClass("btn btn-secondary history-button")
+      .text(city);
+   console.log(historyButton);
+   // Append (prepend to start) the button to the search history container
+   $("#history").prepend(historyButton);
    getCurrentWeather(city);
    getWeatherForecast(city);
-   
+
 });
 // Event listener for the clear history button
 $("#clear-history-button").on("click", function () {
@@ -107,62 +81,65 @@ $("#clear-history-button").on("click", function () {
    $("#history").find(".history-button").remove();
 });
 
-
+//current weather conditions
 function getCurrentWeather(cityName) {
-   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=metric" 
+   //weather in url
+   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=metric"
 
    console.log(queryURL);
    console.log("getCurrentWeather clicked");
-   
-   fetch (queryURL)
-   .then(function(response){
-      return response.json();
-   }).then(function(data){ //waiting for response to be returned to json. can give any name. data is what comes back from the fetch url
-     var sectionToday = $("#today");
-  
-   sectionToday.empty();
 
-      // make Bootstrap card
-      var card = $("<div>").addClass("card");
-      // make card body
-      var cardBody = $("<div>").addClass("card-body");
-      // card title with the city name
-      var cardTitle = $("<h1>").addClass("card-title").text( cityName + " " + rightNow);
+   fetch(queryURL)
+      .then(function (response) {
+         return response.json();
+      }).then(function (data) { //waiting for response to be returned to json. can give any name. data is what comes back from the fetch url
+         var sectionToday = $("#today");
 
-      // variables for info
-      var temperature = data.main.temp;
-      var humidity = data.main.humidity;
-      var windSpeed = data.wind.speed;
-      var iconCode = data.weather[0].icon;
+         sectionToday.empty();
+         //code worked through with tutor
+         // make Bootstrap card
+         var card = $("<div>").addClass("card");
+         // make card body
+         var cardBody = $("<div>").addClass("card-body");
+         // card title with the city name
+         var cardTitle = $("<h1>").addClass("card-title").text(cityName + " " + rightNow);
 
-      console.log("wind speed" + windSpeed);
+         // variables for info
+         var temperature = data.main.temp;
+         var humidity = data.main.humidity;
+         var windSpeed = data.wind.speed;
+         var iconCode = data.weather[0].icon;
 
-      // Creates paragraphs for the weather info
-      var temperatureParagraph = $("<p>").text("Temperature: " + temperature + " °C");
-      var humidityParagraph = $("<p>").text("Humidity: " + humidity + "%");
-      var windSpeedParagraph = $("<p>").text("Wind Speed: " + windSpeed + " m/s");
-      var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
-      var iconImage = $("<img>").attr("src", iconUrl).attr("alt", "Weather Icon");
+         console.log("wind speed" + windSpeed);
 
-
-      // Appends elements to the card body
-      cardBody.append(cardTitle, iconImage, temperatureParagraph, humidityParagraph, windSpeedParagraph);
-
-      // Append card body to the card
-      card.append(cardBody);
-
-      // Appends the card to the "today" html
-      sectionToday.append(card);
+         // Creates paragraphs for the weather info
+         var temperatureParagraph = $("<p>").text("Temperature: " + temperature + " °C");
+         var humidityParagraph = $("<p>").text("Humidity: " + humidity + "%");
+         var windSpeedParagraph = $("<p>").text("Wind Speed: " + windSpeed + " m/s");
+         var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+         var iconImage = $("<img>").attr("src", iconUrl).attr("alt", "Weather Icon");
 
 
-      console.log(data);
-    
-   })
-   .catch(function(error) {
-      console.log("Error fetching weather data: " + error);
-   });
+         // Appends elements to the card body
+         cardBody.append(cardTitle, iconImage, temperatureParagraph, humidityParagraph, windSpeedParagraph);
+
+         // Append card body to the card
+         card.append(cardBody);
+
+         // Appends the card to the "today" html
+         sectionToday.append(card);
+
+
+         console.log(data);
+
+      })
+      //catch we talked about in class. Need to research this more.
+      .catch(function (error) {
+         console.log("Error fetching weather data: " + error);
+      });
 }
 
+//copies functiom above but with for loop
 function getWeatherForecast(cityName) {
    var sectionForecast = $("#forecast");
    var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=metric";
@@ -170,62 +147,63 @@ function getWeatherForecast(cityName) {
    console.log("getWeatherForecast clicked");
 
    fetch(queryURLForecast)
-       .then(function (response) {
-           return response.json();
-       }).then(function (data) {
-           sectionForecast.empty();
+      .then(function (response) {
+         return response.json();
+      }).then(function (data) {
+         //makes sure only 1 at a time on pagr
+         sectionForecast.empty();
 
-           // Create a Bootstrap row
-           var forecastRow = $("<div>").addClass("row");
+         // Create a Bootstrap row
+         var forecastRow = $("<div>").addClass("row");
+         //method from class activity
+         // Loop through the forecast data to get every 8th entry
+         for (var i = 0; i < data.list.length; i += 8) {
+            var forecastData = data.list[i];
+            var forecastDate = dayjs(forecastData.dt_txt).format('D/MM/YY');
+            var forecastIconCode = forecastData.weather[0].icon;
+            var forecastMinTemp = forecastData.main.temp_min;
+            var forecastMaxTemp = forecastData.main.temp_max;
+            var forecastHumidity = forecastData.main.humidity;
 
-           // Loop through the forecast data to get every 8th entry
-           for (var i = 0; i < data.list.length; i += 8) {
-               var forecastData = data.list[i];
-               var forecastDate = dayjs(forecastData.dt_txt).format('D/MM/YY');
-               var forecastIconCode = forecastData.weather[0].icon;
-               var forecastMinTemp = forecastData.main.temp_min;
-               var forecastMaxTemp = forecastData.main.temp_max;
-               var forecastHumidity = forecastData.main.humidity;
+            console.log("new forecast data" + forecastData);
 
-               console.log("new forecast data" + forecastData);
+            // Create Bootstrap column
+            var cardColumn = $("<div>").addClass("container fluid col-md-2");
 
-               // Create Bootstrap column
-               var cardColumn = $("<div>").addClass("col-md-2");
+            // Create Bootstrap card
+            var card = $("<div>").addClass("card mb-2 forecastcard");
+            // Create card body
+            var cardBody = $("<div>").addClass("card-body");
+            // Card title with the date
+            var cardTitle = $("<h5>").addClass("card-title").text(forecastDate);
+            // Create an image element for the weather icon
+            var iconUrl = "https://openweathermap.org/img/w/" + forecastIconCode + ".png";
+            var iconImage = $("<img>").attr("src", iconUrl).attr("alt", "Weather Icon");
+            // Create paragraphs for the weather info
+            var minTempParagraph = $("<p>").text("Min Temp: " + forecastMinTemp + " °C");
+            var maxTempParagraph = $("<p>").text("Max Temp: " + forecastMaxTemp + " °C");
+            var humidityParagraph = $("<p>").text("Humidity: " + forecastHumidity + "%");
 
-               // Create Bootstrap card
-               var card = $("<div>").addClass("card mb-2 forecastcard");
-               // Create card body
-               var cardBody = $("<div>").addClass("card-body");
-               // Card title with the date
-               var cardTitle = $("<h5>").addClass("card-title").text(forecastDate);
-               // Create an image element for the weather icon
-               var iconUrl = "https://openweathermap.org/img/w/" + forecastIconCode + ".png";
-               var iconImage = $("<img>").attr("src", iconUrl).attr("alt", "Weather Icon");
-               // Create paragraphs for the weather info
-               var minTempParagraph = $("<p>").text("Min Temp: " + forecastMinTemp + " °C");
-               var maxTempParagraph = $("<p>").text("Max Temp: " + forecastMaxTemp + " °C");
-               var humidityParagraph = $("<p>").text("Humidity: " + forecastHumidity + "%");
+            // Appends elements to the card body
+            cardBody.append(cardTitle, iconImage, minTempParagraph, maxTempParagraph, humidityParagraph);
+            // Append card body to the card
+            card.append(cardBody);
 
-               // Appends elements to the card body
-               cardBody.append(cardTitle, iconImage, minTempParagraph, maxTempParagraph, humidityParagraph);
-               // Append card body to the card
-               card.append(cardBody);
+            // Appends the card to the column
+            cardColumn.append(card);
 
-               // Append the card to the column
-               cardColumn.append(card);
+            // Appends the column to the row
+            forecastRow.append(cardColumn);
+         }
 
-               // Append the column to the row
-               forecastRow.append(cardColumn);
-           }
+         // Appends the row to the "forecast" section
+         sectionForecast.append(forecastRow);
 
-           // Appends the row to the "forecast" section
-           sectionForecast.append(forecastRow);
-
-           console.log(data);
-       })
-       .catch(function (error) {
-           console.log("Error fetching forecast data: " + error);
-       });
+         console.log(data);
+      })
+      .catch(function (error) {
+         console.log("Error fetching forecast data: " + error);
+      });
 }
 
 
